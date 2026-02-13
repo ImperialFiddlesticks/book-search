f0696b830a4c27878d7be2a8070d5cc5813a27a6
ImperialFiddlesticks <bleckert.andrea@gmail.com>
2026-02-13 13:11:47 +0100

    Added QueryClient

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,20 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   return (
-    <PaperProvider
-      settings={{
-        icon: (props) => <MaterialCommunityIcons {...props} />,
-      }}
-    >
-      <Stack />
-    </PaperProvider>
+    <QueryClientProvider client={queryClient}>
+      <PaperProvider
+        settings={{
+          icon: (props) => <MaterialCommunityIcons {...props} />,
+        }}
+      >
+        <Stack />
+      </PaperProvider>
+    </QueryClientProvider>
   );
 }
