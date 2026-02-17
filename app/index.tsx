ba9f90f34319f4f179424f77d1734c2b9b2ea9ba
ImperialFiddlesticks <bleckertandrea@gmail.com>
2026-02-17 10:00:31 +0100

    Added Saved Books to index, and added placeholder books for easier styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,111 @@
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
+import { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Booksearchbar from "./components/Booksearchbar/Booksearchbar";
+import SavedBookBar from "./components/Booksearchbar/SavedBookBar";
+import { useFavoritesStore } from "./store/favoritesStore";
+import { Book } from "./types/bookProps";
+
+//placeholder böcker för att kunna styla
+const PLACEHOLDER_BOOKS: Book[] = [
+  {
+    key: "1",
+    title: "The Great Gatsby",
+    author_name: ["F. Scott Fitzgerald"],
+    first_publish_year: 1925,
+    cover_i: 153747,
+  },
+  {
+    key: "2",
+    title: "To Kill a Mockingbird",
+    author_name: ["Harper Lee"],
+    first_publish_year: 1960,
+    cover_i: 8228691,
+  },
+  {
+    key: "3",
+    title: "1984",
+    author_name: ["George Orwell"],
+    first_publish_year: 1949,
+    cover_i: 8575708,
+  },
+  {
+    key: "4",
+    title: "Dune",
+    author_name: ["Frank Herbert"],
+    first_publish_year: 1965,
+    cover_i: 6895512,
+  },
+  {
+    key: "5",
+    title: "The Great Gatsby",
+    author_name: ["F. Scott Fitzgerald"],
+    first_publish_year: 1925,
+    cover_i: 153747,
+  },
+  {
+    key: "6",
+    title: "To Kill a Mockingbird",
+    author_name: ["Harper Lee"],
+    first_publish_year: 1960,
+    cover_i: 8228691,
+  },
+  {
+    key: "7",
+    title: "1984",
+    author_name: ["George Orwell"],
+    first_publish_year: 1949,
+    cover_i: 8575708,
+  },
+  {
+    key: "8",
+    title: "Dune",
+    author_name: ["Frank Herbert"],
+    first_publish_year: 1965,
+    cover_i: 6895512,
+  },
+  {
+    key: "9",
+    title: "The Great Gatsby",
+    author_name: ["F. Scott Fitzgerald"],
+    first_publish_year: 1925,
+    cover_i: 153747,
+  },
+  {
+    key: "10",
+    title: "To Kill a Mockingbird",
+    author_name: ["Harper Lee"],
+    first_publish_year: 1960,
+    cover_i: 8228691,
+  },
+  {
+    key: "11",
+    title: "1984",
+    author_name: ["George Orwell"],
+    first_publish_year: 1949,
+    cover_i: 8575708,
+  },
+  {
+    key: "12",
+    title: "Dune",
+    author_name: ["Frank Herbert"],
+    first_publish_year: 1965,
+    cover_i: 6895512,
+  },
+];
 
 export default function Home() {
+  const router = useRouter();
+  const { favorites, isSaved, toggleFavorite, loadFavorites } =
+    useFavoritesStore();
+
+  useEffect(() => {
+    loadFavorites();
+  }, []);
+  const handleBookPress = (book: Book) => {
+    router.push("/details");
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home Screen</Text>
@@ -13,6 +116,15 @@ export default function Home() {
       <Link href="/details">
         <Text style={styles.link}>Book Details</Text>
       </Link>
+      <SavedBookBar
+        // books={favorites}
+        books={PLACEHOLDER_BOOKS}
+        onBookPress={handleBookPress}
+        isSaved={() => true}
+        onToggle={() => {}}
+        // isSaved={isSaved}
+        // onToggle={toggleFavorite}
+      />
     </View>
   );
 }
