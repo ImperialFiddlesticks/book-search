bdfd9dc9b3748ec1aadbb02ca29a2366d6b35516
Anna Groshkova <anna.matvyeyenko@gmail.com>
2026-02-26 19:08:47 +0100

    formatting

--- app/store/collectionsStore.ts
+++ app/store/collectionsStore.ts
@@ -1,53 +1,53 @@
-import { Book } from "../types/bookProps"
+import { Book } from "../types/bookProps";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
 
 interface CollectionsStore {
-    collections: Collection[]
-    loadCollections: () => Promise<void>
-    addNewCollection: (title: string, books?: Book[]) => void
-    deleteCollection?: (title: string) => void
-    updateCollection?: (title: string, books: Book[]) => void
+  collections: Collection[];
+  loadCollections: () => Promise<void>;
+  addNewCollection: (title: string, books?: Book[]) => void;
+  deleteCollection?: (title: string) => void;
+  updateCollection?: (title: string, books: Book[]) => void;
 }
 
 interface Collection {
-    title: string
-    books: Book[]
+  title: string;
+  books: Book[];
 }
 
-export const useColllectionsStore = create<CollectionsStore>()((set, get) => ({
+export const useCollectionsStore = create<CollectionsStore>()((set, get) => ({
   collections: [] as Collection[],
 
-    loadCollections: async () => {
+  loadCollections: async () => {
     try {
-      const storedCollections = await AsyncStorage.getItem("collections");
-      if (storedCollections !== null) {
-        set({ collections: JSON.parse(storedCollections) });
-      }
+      const storedCollections =
+        (await AsyncStorage.getItem("collections")) || "[]";
+      set({ collections: JSON.parse(storedCollections) });
     } catch (error) {
       console.error("Failed to load collections", error);
     }
   },
 
   addNewCollection: (title, books) => {
-    const currentCollections = get().collections
-    const updatedCollections = [...currentCollections, { title, books: books ?? [] }]
+    const currentCollections = get().collections;
+    const updatedCollections = [
+      ...currentCollections,
+      { title, books: books ?? [] },
+    ];
 
-    set({collections: updatedCollections});
+    set({ collections: updatedCollections });
 
     AsyncStorage.setItem("collections", JSON.stringify(updatedCollections));
   },
-
 
   deleteCollection: (title) => {
-    const currentCollections = get().collections
-    const updatedCollections = currentCollections.filter(c => c.title !== title)
+    const currentCollections = get().collections;
+    const updatedCollections = currentCollections.filter(
+      (c) => c.title !== title,
+    );
 
-    set({collections: updatedCollections});
+    set({ collections: updatedCollections });
 
     AsyncStorage.setItem("collections", JSON.stringify(updatedCollections));
   },
-
-
 }));
-
